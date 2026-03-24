2126f32bb596e64f35c82692a537754d8dd572b3
barakshamir <barak20000@gmail.com>
2026-03-24 21:16:21 +0200

    f

--- cursor-automation/automation-deepfake/src/test/java/com/is/deepfake/tests/ui/LoginPageTest.java
+++ cursor-automation/automation-deepfake/src/test/java/com/is/deepfake/tests/ui/LoginPageTest.java
@@ -1,10 +1,13 @@
 package com.is.deepfake.tests.ui;
 
+import org.testng.annotations.Test;
+
 import com.is.common.pages.CommonLoginPage;
+import com.is.common.testng.annotation.CommonAnnotation;
 import com.is.deepfake.config.DeepFakeConfig;
 import com.is.deepfake.selenium.pages.NextPage;
 import com.is.deepfake.testng.DeepfakeBaseTest;
-import org.testng.annotations.Test;
+import com.is.deepfake.testng.annotation.DeepfakeAnnotation;
 
 /**
  * UI test: login via DemoTool login page.
@@ -13,6 +16,9 @@ import org.testng.annotations.Test;
 public class LoginPageTest extends DeepfakeBaseTest {
 
     @Test(description = "Login to DemoTool")
+    @DeepfakeAnnotation(createDfsTenant = true, joinTeamsMeeting = true)
+    @CommonAnnotation(createCompany = true)
+    
     public void login() {
         DeepFakeConfig cfg = DeepFakeConfig.get();
         new CommonLoginPage()
